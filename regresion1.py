fc20f63020a96b32de9234e41f52147955a95b2d
itsjaviervivas <javier.vivas@urosario.edu.co>
2021-10-11 21:00:28 -0500

    Se agrego matriz x

--- regresion1.py
+++ regresion1.py
@@ -9,7 +9,7 @@ Created on Mon Oct 11 18:36:26 2021
 import pandas as pd
 import numpy as np
 
-
+#revisar si es posible dejarla a un lado
 def pregunta():
     numexp =  int(input("Ingrese el número de variables explicativas que desse para su modelo: ")) #El numero de variables explicativas
     empty = [] #lista vacia
@@ -20,10 +20,20 @@ def pregunta():
 
 def estimarBetas (data, datax, datay):
     """Vamos a estimar las Betas de la regresión lineal, mediante el método de minimos cuadrados."""
-    pregunta()
+    numexp =  int(input("Ingrese el número de variables explicativas que desse para su modelo: ")) #El numero de variables explicativas
+    empty = [] #lista vacia
+    for i in range(numexp): #Le pregunta al usuario el nombre de las variables
+          b = input("Por favor, ingrese el nombre de la variable explicativa: ")
+          empty.append(b)
+    print("Estas son las variables a considerar para su modelo: ", empty)
+    datax = [] #la matriz con nuestras variables explicativas
+    for i in empty:
+        if i in data:
+            datax.append(i) #las va agregando de una a una
+        elif i not in data:
+            print("Revisar si el nombre existe.")
+    #mirar como usar esta matriz con la libreria pandas para poder multiplicarla 
     
     
     
-
-estimarBetas(1, 2, 3)
     
